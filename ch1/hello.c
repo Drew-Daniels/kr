28c8e65bb2d38e2ed4595a53414e03af949088a0
Drew Daniels <daniels_drew@protonmail.com>
2022-07-07 08:09:06 -0500

    Now using a for loop to print table.

--- ch1/hello.c
+++ ch1/hello.c
@@ -6,30 +6,9 @@
 */
 main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
+    int fahr;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
     printf("Farenheit => Celsius Table\n");
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-
-    printf("Celsius => Farenheit Table\n");
-    /* Add logic here */
-    int cLower, cUpper, cStep;
-    cLower = 0;
-    cUpper = 300;
-    celsius = cLower;
-    while (celsius <= cUpper) {
-        fahr = ((9.0 / 5.0) * celsius) + 32.0;
-        printf("%3.1f\t%6.0f\n", celsius, fahr);
-        celsius = celsius + 20;
-    }
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
 }
