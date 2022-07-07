a60de51e85f65f37e9e37d0c26889e8593c01826
Drew Daniels <daniels_drew@protonmail.com>
2022-07-06 22:33:48 -0500

    Added a header.

--- ch1/hello.c
+++ ch1/hello.c
@@ -14,6 +14,7 @@ main()
     step = 20;
 
     fahr = lower;
+    printf("TEMPERATURE TABLE\n");
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
